43e71531a7843cf6d3950988618288718a2a28f5
Daniel Kostro <kostro.d@gmail.com>
2016-07-20 16:31:39 +0200

    implementation and a few tests

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # combinations
+
+  [![NPM version][npm-image]][npm-url]
+  [![build status][travis-image]][travis-url]
+  [![David deps][david-image]][david-url]
+  [![npm download][download-image]][download-url]
+  
 Generate all possible unordered samples of size m, without replacement, from a set of n objects
+
+## Installation
+
+```
+$ npm install ml-combinations
+```
+
+## References
+Phillip J Chase, `Algorithm 382: Combinations of M out of N Objects \[G6\]',
+  Communications of the Association for Computing Machinery 13:6:368 (1970).
+  [To the article](http://dx.doi.org/10.1145/362384.362502)
+## License
+
+[MIT](./LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/ml-combinations.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/ml-combinations
+[travis-image]: https://img.shields.io/travis/mljs/combinations/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/mljs/combinations
+[david-image]: https://img.shields.io/david/mljs/combinations.svg?style=flat-square
+[david-url]: https://david-dm.org/mljs/combinations
+[download-image]: https://img.shields.io/npm/dm/ml-combinations.svg?style=flat-square
+[download-url]: https://npmjs.org/package/ml-combinations
---
+++ src/index.js
@@ -0,0 +1,91 @@
+'use strict';
+
+
+
+module.exports = function *(M, N, options = {mode: 'index'}) {
+    var a = new Array(N);
+    var c = new Array(M);
+    var b = new Array(N);
+    var p = new Array(N + 2);
+    var x, y, z;
+
+    // init a and b
+    for (var i = 0; i < N; i++) {
+        a[i] = i;
+        if (i < N - M) b[i] = 0;
+        else b[i] = 1;
+    }
+
+    // init c
+    for (i = 0; i < M; i++) {
+        c[i] = N - i - 1;
+    }
+
+    // init p
+    for (i = 0; i < p.length; i++) {
+        if (i === 0) p[i] = N + 1;
+        else if (i <= N - M) p[i] = 0;
+        else if (i <= N) p[i] = i - N + M;
+        else p[i] = -2;
+    }
+
+    function twiddle() {
+        var i, j, k;
+        j = 1;
+        while (p[j] <= 0)
+            j++;
+        if (p[j - 1] === 0) {
+            for (i = j - 1; i !== 1; i--)
+                p[i] = -1;
+            p[j] = 0;
+            x = z = 0;
+            p[1] = 1;
+            y = j - 1;
+        } else {
+            if (j > 1)
+                p[j - 1] = 0;
+            do
+                j++;
+            while (p[j] > 0);
+            k = j - 1;
+            i = j;
+            while (p[i] === 0)
+                p[i++] = -1;
+            if (p[i] === -1) {
+                p[i] = p[k];
+                z = p[k] - 1;
+                x = i - 1;
+                y = k - 1;
+                p[k] = -1;
+            } else {
+                if (i === p[0]) {
+                    return 0;
+                } else {
+                    p[j] = p[i];
+                    z = p[i] - 1;
+                    p[i] = 0;
+                    x = j - 1;
+                    y = i - 1;
+                }
+            }
+        }
+        return 1;
+    }
+
+    if (options.mode === 'index') {
+        yield c.slice();
+        while (twiddle()) {
+            c[z] = a[x];
+            yield c.slice();
+        }
+    } else if (options.mode === 'mask') {
+        yield b.slice();
+        while (twiddle()) {
+            b[x] = 1;
+            b[y] = 0;
+            yield b.slice();
+        }
+    } else {
+        throw new Error('Invalid mode');
+    }
+};
