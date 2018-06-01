d22266083cec8d77f754a4b3e8f85807e871e2c6
Daniel Kostro <kostro.d@gmail.com>
2018-06-01 16:03:29 +0200

    should.js -> expect

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -4,7 +4,7 @@ const combinations = require('..');
 describe('Compute combinations', function () {
   it('should generate 1 of N combinations (index mode)', function () {
     var options = { mode: 'index' };
-    Array.from(combinations(1, 8, options)).should.eql([
+    expect(Array.from(combinations(1, 8, options))).toEqual([
       [7],
       [0],
       [1],
@@ -14,26 +14,26 @@ describe('Compute combinations', function () {
       [5],
       [6]
     ]);
-    Array.from(combinations(1, 1)).should.eql([[0]]);
-    Array.from(combinations(1, 2)).should.eql([[1], [0]]);
+    expect(Array.from(combinations(1, 1))).toEqual([[0]]);
+    expect(Array.from(combinations(1, 2))).toEqual([[1], [0]]);
   });
 
   it('should generate 1 of N combinations (mask mode)', function () {
     var options = { mode: 'mask' };
-    Array.from(combinations(1, 1, options)).should.eql([[1]]);
-    Array.from(combinations(1, 2, options)).should.eql([[0, 1], [1, 0]]);
+    expect(Array.from(combinations(1, 1, options))).toEqual([[1]]);
+    expect(Array.from(combinations(1, 2, options))).toEqual([[0, 1], [1, 0]]);
   });
 
   it('Invalid mode should throw', function () {
     var options = { mode: 'invalid' };
-    (function () {
+    expect(function () {
       combinations(1, 10, options).next();
-    }.should.throw(/Invalid mode/));
+    }).toThrowError(/Invalid mode/);
   });
 
   it('should generate some more complicated comibinations', function () {
-    Array.from(combinations(2, 3)).should.eql([[1, 2], [0, 2], [0, 1]]);
-    Array.from(combinations(2, 4)).should.eql([
+    expect(Array.from(combinations(2, 3))).toEqual([[1, 2], [0, 2], [0, 1]]);
+    expect(Array.from(combinations(2, 4))).toEqual([
       [2, 3],
       [0, 3],
       [1, 3],
