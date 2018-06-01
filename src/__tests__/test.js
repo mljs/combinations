098831fccc899a3658554bac83e5e33ad0bddcfd
Daniel Kostro <kostro.d@gmail.com>
2018-06-01 15:57:32 +0200

    upgrade packages and use jest

---
+++ src/__tests__/test.js
@@ -0,0 +1,45 @@
+'use strict';
+const combinations = require('..');
+
+describe('Compute combinations', function () {
+  it('should generate 1 of N combinations (index mode)', function () {
+    var options = { mode: 'index' };
+    Array.from(combinations(1, 8, options)).should.eql([
+      [7],
+      [0],
+      [1],
+      [2],
+      [3],
+      [4],
+      [5],
+      [6]
+    ]);
+    Array.from(combinations(1, 1)).should.eql([[0]]);
+    Array.from(combinations(1, 2)).should.eql([[1], [0]]);
+  });
+
+  it('should generate 1 of N combinations (mask mode)', function () {
+    var options = { mode: 'mask' };
+    Array.from(combinations(1, 1, options)).should.eql([[1]]);
+    Array.from(combinations(1, 2, options)).should.eql([[0, 1], [1, 0]]);
+  });
+
+  it('Invalid mode should throw', function () {
+    var options = { mode: 'invalid' };
+    (function () {
+      combinations(1, 10, options).next();
+    }.should.throw(/Invalid mode/));
+  });
+
+  it('should generate some more complicated comibinations', function () {
+    Array.from(combinations(2, 3)).should.eql([[1, 2], [0, 2], [0, 1]]);
+    Array.from(combinations(2, 4)).should.eql([
+      [2, 3],
+      [0, 3],
+      [1, 3],
+      [1, 2],
+      [0, 2],
+      [0, 1]
+    ]);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,98 +1,98 @@
 'use strict';
 const defaultOptions = {
-    mode: 'index'
+  mode: 'index'
 };
 
 module.exports = function *(M, N, options) {
-    options = Object.assign({}, defaultOptions, options);
-    var a = new Array(N);
-    var c = new Array(M);
-    var b = new Array(N);
-    var p = new Array(N + 2);
-    var x, y, z;
+  options = Object.assign({}, defaultOptions, options);
+  var a = new Array(N);
+  var c = new Array(M);
+  var b = new Array(N);
+  var p = new Array(N + 2);
+  var x, y, z;
 
-    // init a and b
-    for (var i = 0; i < N; i++) {
-        a[i] = i;
-        if (i < N - M) b[i] = 0;
-        else b[i] = 1;
+  // init a and b
+  for (var i = 0; i < N; i++) {
+    a[i] = i;
+    if (i < N - M) b[i] = 0;
+    else b[i] = 1;
+  }
+
+  // init c
+  for (i = 0; i < M; i++) {
+    c[i] = N - M + i;
+  }
+
+  // init p
+  for (i = 0; i < p.length; i++) {
+    if (i === 0) p[i] = N + 1;
+    else if (i <= N - M) p[i] = 0;
+    else if (i <= N) p[i] = i - N + M;
+    else p[i] = -2;
+  }
+
+  function twiddle() {
+    var i, j, k;
+    j = 1;
+    while (p[j] <= 0) {
+      j++;
     }
-
-    // init c
-    for (i = 0; i < M; i++) {
-        c[i] = N - M + i;
-    }
-
-    // init p
-    for (i = 0; i < p.length; i++) {
-        if (i === 0) p[i] = N + 1;
-        else if (i <= N - M) p[i] = 0;
-        else if (i <= N) p[i] = i - N + M;
-        else p[i] = -2;
-    }
-
-    function twiddle() {
-        var i, j, k;
-        j = 1;
-        while (p[j] <= 0) {
-            j++;
-        }
-        if (p[j - 1] === 0) {
-            for (i = j - 1; i !== 1; i--) {
-                p[i] = -1;
-            }
-            p[j] = 0;
-            x = z = 0;
-            p[1] = 1;
-            y = j - 1;
-        } else {
-            if (j > 1) {
-                p[j - 1] = 0;
-            }
-            do {
-                j++;
-            }
-            while (p[j] > 0);
-            k = j - 1;
-            i = j;
-            while (p[i] === 0) {
-                p[i++] = -1;
-            }
-            if (p[i] === -1) {
-                p[i] = p[k];
-                z = p[k] - 1;
-                x = i - 1;
-                y = k - 1;
-                p[k] = -1;
-            } else {
-                if (i === p[0]) {
-                    return 0;
-                } else {
-                    p[j] = p[i];
-                    z = p[i] - 1;
-                    p[i] = 0;
-                    x = j - 1;
-                    y = i - 1;
-                }
-            }
-        }
-        return 1;
-    }
-
-    if (options.mode === 'index') {
-        yield c.slice();
-        while (twiddle()) {
-            c[z] = a[x];
-            yield c.slice();
-        }
-    } else if (options.mode === 'mask') {
-        yield b.slice();
-        while (twiddle()) {
-            b[x] = 1;
-            b[y] = 0;
-            yield b.slice();
-        }
+    if (p[j - 1] === 0) {
+      for (i = j - 1; i !== 1; i--) {
+        p[i] = -1;
+      }
+      p[j] = 0;
+      x = z = 0;
+      p[1] = 1;
+      y = j - 1;
     } else {
-        throw new Error('Invalid mode');
+      if (j > 1) {
+        p[j - 1] = 0;
+      }
+      do {
+        j++;
+      }
+      while (p[j] > 0);
+      k = j - 1;
+      i = j;
+      while (p[i] === 0) {
+        p[i++] = -1;
+      }
+      if (p[i] === -1) {
+        p[i] = p[k];
+        z = p[k] - 1;
+        x = i - 1;
+        y = k - 1;
+        p[k] = -1;
+      } else {
+        if (i === p[0]) {
+          return 0;
+        } else {
+          p[j] = p[i];
+          z = p[i] - 1;
+          p[i] = 0;
+          x = j - 1;
+          y = i - 1;
+        }
+      }
     }
+    return 1;
+  }
+
+  if (options.mode === 'index') {
+    yield c.slice();
+    while (twiddle()) {
+      c[z] = a[x];
+      yield c.slice();
+    }
+  } else if (options.mode === 'mask') {
+    yield b.slice();
+    while (twiddle()) {
+      b[x] = 1;
+      b[y] = 0;
+      yield b.slice();
+    }
+  } else {
+    throw new Error('Invalid mode');
+  }
 };
